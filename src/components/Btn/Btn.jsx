83083d4e03f20b6009e9621339bd741e41c52bc3
MarsGotBars <marcinm@live.nl>
2024-02-28 12:03:16 +0100

    QoL for the buttons

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Btn({onClick, classes = "", children}) {
+export default function Btn({disabled, onClick, classes = "", children}) {
   return (
-    <button onClick={onClick} className={`text-white hover:bg-white hover:text-slate-500 duration-200 rounded-sm ${classes}`}>{children}</button>
+    <button disabled={disabled} onClick={onClick} className={`text-white enabled:hover:bg-white enabled:cursor-pointer cursor-not-allowed enabled:hover:text-slate-500 duration-200 rounded-sm ${classes}`}>{children}</button>
   )
 }
