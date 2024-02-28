efab65f4c47bd19d08281f41d3b334547bdebe76
MarsGotBars <marcinm@live.nl>
2024-02-28 16:42:24 +0100

    fixed a random tailwind issue that popped up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const spanSafeList = []
+const num = 12
+for(let i = 0; i < 12; i++){
+  spanSafeList.push(`col-span-${i}`)
+}
 module.exports = {
+  safelist: spanSafeList,
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
