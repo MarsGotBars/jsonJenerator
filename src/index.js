2894286df4856f6d71c19ea344ad9d4163fe41a2
MarsGotBars <marcinm@live.nl>
2024-02-28 10:08:49 +0100

    initial react query commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
-
